338c127ddb449c69ec96416238eaea0d795eae8e
ajithanalluri <44363166+ajithanalluri@users.noreply.github.com>
2021-11-08 13:47:54 +0530

    Update EshopMessagingServiceApplication.java

--- microdemo/eshop-messaging-service/src/main/java/com/example/eshopmessagingservice/EshopMessagingServiceApplication.java
+++ microdemo/eshop-messaging-service/src/main/java/com/example/eshopmessagingservice/EshopMessagingServiceApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class EshopMessagingServiceApplication {
+    
+    static final java.util.logging.Logger log = Logger.getLogger(EshopMessagingServiceApplication.class.getName());
+
     public static void main(String[] args) {
         SpringApplication.run(EshopMessagingServiceApplication.class, args);
+        log.info("EshopMessagingServiceApplication ...");
+
     }
 }
